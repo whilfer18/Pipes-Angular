3db8c2b0d3d234d2d90631e23a7c85546d804ff5
whilfer18 <whilferrivera@gmail.com>
2020-04-20 21:31:08 -0500

    Pipess whilmer

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Whilmer Fernández';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  personajes: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  salario: number = 1234.5
+  fecha: Date = new Date();
+  activar: boolean = true;
+
+  idioma: string = 'en';
+  videoUrl: string = 'https://www.youtube.com/embed/-CBKW-90VxQ';
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
 }
